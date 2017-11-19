9ea7e9d302a7a52ed845fc9dd58772a17310daae
Ali Raza <aliraza1567@gmail.com>
2017-11-19 02:32:37 +0100

    Cities Controller

---
+++ CityInformation.Api/CityInformation.Api/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CityInformation.Api.Controllers
+{
+    public class CitiesController: Controller
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id=1, Name = "Amsterdam"},
+                    new {id=2, Name = "New York"}
+                }
+                );
+        }
+    }
+}
--- CityInformation.Api/CityInformation.Api/Startup.cs
+++ CityInformation.Api/CityInformation.Api/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +11,7 @@ namespace CityInformation.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,11 +23,17 @@ namespace CityInformation.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler();
+            }
+
+            app.UseMvc();
+            
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
